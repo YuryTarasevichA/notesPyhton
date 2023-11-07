c71d7820d96bd390a0d019a853ec8a1cc0acff60
Yura <yura.tarasevichunt@gmail.com>
2023-11-07 19:40:40 +0300

    Функция для добавления новой заметки

--- Program.py
+++ Program.py
@@ -10,3 +10,19 @@ def load_notes():
     except FileNotFoundError:
         notes = []
     return notes
+
+
+
+# Функция для добавления новой заметки
+def add_note():
+    title = input("Введите заголовок заметки: ")
+    body = input("Введите тело заметки: ")
+    note = {
+        'id': len(notes) + 1,
+        'title': title,
+        'body': body,
+        'created_at': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'updated_at': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    }
+    notes.append(note)
+    print("Заметка успешно сохранена")
